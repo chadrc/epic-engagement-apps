407f3c2872917a697991d5d7598ba0cf565da590
chadrc <chadrcollins91@gmail.com>
2024-10-07 17:19:25 -0400

    Added Datasheet model

--- shared/src/commonMain/kotlin/com/yulepanda/wargamebuilder/Datasheet.kt
+++ shared/src/commonMain/kotlin/com/yulepanda/wargamebuilder/Datasheet.kt
@@ -1,5 +1,38 @@
 package com.yulepanda.wargamebuilder
 
 class Datasheet {
+    var name = ""
+    var skill = 0
+    var health = 0
+    var speed = 0
+    var composition: Array<ModelGroup> = arrayOf(ModelGroup() )
+    var abilities: Array<UnitAbility> = emptyArray()
+    var statTable: RollTable = RollTable()
+}
 
+class UnitAbility {
+    var name = ""
+    var description: String? = null
+}
+
+class ModelGroup {
+    var name = "Model Name"
+    var count = 0
+}
+
+class RollTable {
+    var resultBreaks: Array<Int> = arrayOf(1)
+    var toSave: Array<Int> = arrayOf(4)
+    var toResist: Array<Int>? = null
+    var hardness: Array<Int>? = null
+    var weapons: Array<WeaponTable> = arrayOf(WeaponTable())
+}
+
+class WeaponTable {
+    var name = ""
+    var attacks: Array<Int> = arrayOf(1)
+    var range: Array<Int> = arrayOf(3)
+    var toHit: Array<Int> = arrayOf(4)
+    var damage: Array<Int> = arrayOf(1)
+    var enhancements: Array<String>? = null
 }
