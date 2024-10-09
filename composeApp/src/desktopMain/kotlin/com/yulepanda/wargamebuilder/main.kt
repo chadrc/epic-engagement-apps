89baac8281b0baff9930bd2a8c7f7a303c58b32a
chadrc <chadrcollins91@gmail.com>
2024-10-09 12:44:46 -0400

    Changed default window size

--- composeApp/src/desktopMain/kotlin/com/yulepanda/wargamebuilder/main.kt
+++ composeApp/src/desktopMain/kotlin/com/yulepanda/wargamebuilder/main.kt
@@ -1,12 +1,18 @@
 package com.yulepanda.wargamebuilder
 
+import androidx.compose.ui.unit.DpSize
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import androidx.compose.ui.window.rememberWindowState
 
 fun main() = application {
+    val windowState = rememberWindowState()
+    windowState.size = DpSize(1280.dp, 720.dp)
     Window(
         onCloseRequest = ::exitApplication,
         title = "WargameBuilder",
+        state = windowState
     ) {
         App()
     }
